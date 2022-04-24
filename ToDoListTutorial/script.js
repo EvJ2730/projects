1b4cdf1fef401bd11a4764f8a6c7087c3e710393
EvJ <73695353+EvJ2730@users.noreply.github.com>
2022-04-24 10:28:12 +0200

    JS functionalities
    > added functionalities on all buttons

--- ToDoListTutorial/script.js
+++ ToDoListTutorial/script.js
@@ -1,6 +1,64 @@
-Buy a new gaming laptop
-Complete a pervous task 
-Create a video for YouTube
-Create a new portfolio site
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteBtn = document.querySelector(".footer button");
 
-#68eacc, #497be8 #8e49e8
+inputBox.onkeyup = ()=>{
+    let userData = inputBox.value;
+    if (userData.trim() != 0) {
+        addBtn.classList.add("active");
+    }else{
+        addBtn.classList.remove("active");
+    }
+}
+showTasks();
+
+addBtn.onclick = () =>{
+    let userData = inputBox.value;
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if(getLocalStorage == null){
+        listArr = [];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userData);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+    addBtn.classList.remove("active");
+}
+
+function showTasks(){
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if(getLocalStorage == null){
+        listArr = [];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    const pendingTasks = document.querySelector(".pendingTasks");
+    pendingTasks.textContent = listArr.length;
+    if(listArr.length > 0){
+        deleteBtn.classList.add("active");
+    }else{
+        deleteBtn.classList.remove("active");
+    }
+    let newLiTag = '';
+    listArr.forEach((element, index) => {
+        newLiTag += `<li>${element}<span onclick = "deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+    });
+    todoList.innerHTML = newLiTag;
+    inputBox.value = "";
+}
+
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("New Todo");
+    listArr = JSON.parse(getLocalStorage);
+    listArr.splice(index, 1);
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+}
+
+deleteBtn.onclick = () =>{
+    listArr = [];
+    localStorage.setItem("New Todo", JSON.stringify(listArr));
+    showTasks();
+}
